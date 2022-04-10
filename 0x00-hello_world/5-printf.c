4eab116403961cae8767009fa015e6f5a1d88b53
fghjkb <natnaeltilahun004@gmail.com>
2022-04-10 11:36:45 -0700

    new commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *My hello, ptintf task
+ *Return - return 1 after program execution
+ */
+int main(viod)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
