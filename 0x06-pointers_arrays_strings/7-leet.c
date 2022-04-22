ff4150eebb2e11c14a38532492af9fc83399a7c8
fghjkb <natnaeltilahun004@gmail.com>
2022-04-21 20:38:54 -0700

    task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * leet- translates string tp 1337
+ * @string : pointer to string
+ * Return: return translated string
+ */
+char *leet(char *string)
+{
+int a;
+int b;
+
+char lower[] = "aeotl";
+char upper[] = "AEOTL";
+char numbers[] = "43071";
+
+for (a = 0; string[a] != '\0'; a++)
+{
+for (b = 0; lower[b] != '\0' && upper[b] != '\0'; b++)
+{
+if (string[a] == lower[b] || string[a] == upper[b])
+{
+string[a] = number[b];
+break;
+}
+}
+}
+
+return (string);
+}
